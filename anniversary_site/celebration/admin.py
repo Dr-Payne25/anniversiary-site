6f9d557256ec2348ae5b7e1ccc5b7b544de99f31
DrPayne-25 <alexpayne1125@yahoo.com>
2025-03-07 09:40:24 -0700

    templates added

--- anniversary_site/celebration/admin.py
+++ anniversary_site/celebration/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Photo, MenuItem, AboutUs
 
-# Register your models here.
+# Register the Photo model
+@admin.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date_taken', 'order')
+    list_editable = ('order',)
+    search_fields = ('title', 'caption')
+
+# Register the MenuItem model
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'course')
+    list_filter = ('course',)
+    search_fields = ('name', 'description')
+
+# Register the AboutUs model
+@admin.register(AboutUs)
+class AboutUsAdmin(admin.ModelAdmin):
+    list_display = ('your_name', 'partner_name', 'anniversary_date')
--- anniversary_site/celebration/views.py
+++ anniversary_site/celebration/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from .models import Photo, MenuItem, AboutUs
 
-# Create your views here.
+def home(request):
+    photos = Photo.objects.all()
+    try:
+        about = AboutUs.objects.first()
+        anniversary_date = about.anniversary_date
+    except AboutUs.DoesNotExist:
+        anniversary_date = None
+    
+    return render(request, 'celebration/home.html', {
+        'photos': photos,
+        'anniversary_date': anniversary_date
+    })
+
+def menu(request):
+    appetizers = MenuItem.objects.filter(course='appetizer')
+    mains = MenuItem.objects.filter(course='main')
+    desserts = MenuItem.objects.filter(course='dessert')
+    
+    return render(request, 'celebration/menu.html', {
+        'appetizers': appetizers,
+        'mains': mains,
+        'desserts': desserts
+    })
+
+def about(request):
+    try:
+        about_us = AboutUs.objects.first()
+    except AboutUs.DoesNotExist:
+        about_us = None
+    
+    return render(request, 'celebration/about.html', {
+        'about_us': about_us
+    })
