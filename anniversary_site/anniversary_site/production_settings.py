a98c9566b5ee57e3051ffcf0a5cf3d514db00bb2
DrPayne-25 <alexpayne1125@yahoo.com>
2025-03-07 15:03:11 -0700

    pathing issues

--- anniversary_site/anniversary_site/production_settings.py
+++ anniversary_site/anniversary_site/production_settings.py
@@ -36,7 +36,7 @@ ALLOWED_HOSTS = ['1yearanni-drpayne.pythonanywhere.com']
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'celebration', 'static'),
 ]
 
 # Optional: Additional Production-Specific Security
